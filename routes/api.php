5cb7553e87766b039875efc5f0277988318deb1a
Guerriere <merveillenzoyem2@gmmail.com>
2025-04-23 20:05:05 +0100

    Premier Commit de mon  project Laravel

---
+++ routes/api.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+Route::post('/register', [AuthController::class, 'register'])->name('register');
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum')->name('logout');
+Route::get('/user', [AuthController::class, 'user'])->middleware('auth:sanctum')->name('user');
+
+Route::middleware('auth:sanctum')->group(function () {
+    // Ici, toutes les routes nécessitant une authentification via Sanctum
+    // seront protégées.
+});
